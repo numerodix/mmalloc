c459f4c2e5233a94492239037cb09f1accfc74c3
Martin Matusiak <numerodix@gmail.com>
2020-03-11 18:40:04 +1100

    improve tests to write into mem and checking later

--- tests/api_mallocate_twice.c
+++ tests/api_mallocate_twice.c
@@ -1,24 +1,36 @@
 #include <assert.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "mmalloc.h"
 
 
-int SIZE = 4 << 10;
+uint64_t nums_fst[] = {7, 7, 7, 7, 7, 7, 7};
+uint64_t nums_snd[] = {9, 9, 9, 9, 9, 9, 9};
 
 int main() {
-    // allocate two equal sized buffers
-    char *buffer1 = (char*) mmalloc(SIZE);
+    // allocate a buffer
+    char *buffer1 = (char*) mmalloc(sizeof(nums_fst));
     assert(buffer1 != NULL);
 
-    char *buffer2 = (char*) mmalloc(SIZE);
+    // fill the buffer
+    memcpy(buffer1, nums_fst, sizeof(nums_fst));
+
+    // allocate a second buffer of the same size
+    char *buffer2 = (char*) mmalloc(sizeof(nums_snd));
     assert(buffer2 != NULL);
 
     // the buffers should have different locations
     assert(buffer1 != buffer2);
+
+    // fill the second buffer with a different contents
+    memcpy(buffer2, nums_snd, sizeof(nums_snd));
+
+    // make sure the first buffer wasn't overwritten
+    int res = memcmp(buffer1, nums_fst, sizeof(nums_fst));
+    assert(res == 0);
 
     // de-allocate the buffers
     mfree(buffer1);
--- tests/api_reallocate_bigger_size.c
+++ tests/api_reallocate_bigger_size.c
@@ -7,15 +7,25 @@
 
 char userdata_small[] = "hi there";
 char userdata[] = "hi there, champ!";
+char userdata_unrelated[] = "not the point!";
 
 int main() {
     int res;
 
+    // allocate a buffer
     void *buffer_small = mmalloc(sizeof(userdata_small));
     assert(buffer_small != NULL);
 
     // fill it with a known contents
     memcpy(buffer_small, userdata_small, sizeof(userdata_small));
+
+    // allocate a second buffer just after it to occupy that space after
+    // the first one
+    char *buffer_unrelated = (char*) mmalloc(sizeof(userdata_unrelated));
+    assert(buffer_unrelated != NULL);
+
+    // fill the second buffer
+    memcpy(buffer_unrelated, userdata_unrelated, sizeof(userdata_unrelated));
 
     // when called with a bigger size return a chunk big enough
     void *buffer = mrealloc(buffer_small, sizeof(userdata));
@@ -32,8 +42,9 @@ int main() {
     res = memcmp(buffer, userdata, sizeof(userdata));
     assert(res == 0);
 
-    // de-allocate the buffer
+    // de-allocate the buffers
     mfree(buffer);
+    mfree(buffer_unrelated);
 
     return 0;
 }
