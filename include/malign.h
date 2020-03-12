8efb35450d8b10671d01ec27ce4fe6aad69054df
Martin Matusiak <numerodix@gmail.com>
2020-03-12 22:57:35 +1100

    add next_power_of_2

--- include/malign.h
+++ include/malign.h
@@ -9,3 +9,6 @@ size_t align_location(size_t location, size_t alignment);
 
 // Returns true if `location` is aligned on `alignment`, otherwise return false
 bool is_aligned(size_t location, size_t alignment);
+
+// Returns the next power of 2 up from `num`
+size_t next_power_of_2(size_t num);
--- src/malign.c
+++ src/malign.c
@@ -12,3 +12,19 @@ size_t align_location(size_t location, size_t alignment) {
 bool is_aligned(size_t location, size_t alignment) {
     return (location & (alignment - 1)) == 0;
 }
+
+
+size_t next_power_of_2(size_t num) {
+    // num must be 64bit
+
+    num--;
+    num |= num >> 1;
+    num |= num >> 2;
+    num |= num >> 4;
+    num |= num >> 8;
+    num |= num >> 16;
+    num |= num >> 32;
+    num++;
+
+    return num;
+}
