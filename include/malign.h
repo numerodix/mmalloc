c9b41869f89484d74cfca95945a937b99aab8497
Martin Matusiak <numerodix@gmail.com>
2020-03-14 17:16:59 +1100

    remove obsolete next_power_of_2 and get_standard_size

--- include/malign.h
+++ include/malign.h
@@ -14,11 +14,6 @@ size_t align_location(size_t location, size_t alignment);
 // Returns true if `location` is aligned on `alignment`, otherwise return false
 bool is_aligned(size_t location, size_t alignment);
 
-// Returns the next power of 2 up from `num`
-size_t next_power_of_2(size_t num);
-
-// Returns a standard size large enough to contain `num`
-size_t get_standard_size(size_t num);
 
 // Returns the base 2 exponent (position of the most significant bit) of `size`
 size_t get_base2_exponent(size_t size);
--- src/malign.c
+++ src/malign.c
@@ -19,31 +19,6 @@ bool is_aligned(size_t location, size_t alignment) {
 }
 
 
-size_t next_power_of_2(size_t num) {
-    // num must be 64bit
-
-    num--;
-    num |= num >> 1;
-    num |= num >> 2;
-    num |= num >> 4;
-    num |= num >> 8;
-    num |= num >> 16;
-    num |= num >> 32;
-    num++;
-
-    return num;
-}
-
-
-size_t get_standard_size(size_t num) {
-    if (num < MIN_STD_SIZE) {
-        return MIN_STD_SIZE;
-    }
-
-    return next_power_of_2(num);
-}
-
-
 size_t get_base2_exponent(size_t size) {
     int counter = 0;
 
